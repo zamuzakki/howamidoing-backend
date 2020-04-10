5e942dfd5411f3d5cf4cfff2dee9020709731e47
Zulfikar Akbar Muzakki <age.zakki@gmail.com>
2020-04-10 17:27:04 +0700

    Edit users.test.test_views, add test for admin user

--- project/users/test/test_views.py
+++ project/users/test/test_views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from faker import Faker
 import factory
 from ..models import User
-from .factories import UserFactory
+from .factories import UserFactory, UserAdminFactory
 
 fake = Faker()
 
@@ -18,19 +18,27 @@ class TestUserListTestCase(APITestCase):
 
     def setUp(self):
         self.url = reverse('user-list')
-        self.user_data = factory.build(dict, FACTORY_CLASS=UserFactory)
+        self.status_data = factory.build(dict, FACTORY_CLASS=UserFactory)
 
-    def test_post_request_with_no_data_fails(self):
+    def test_create_user_with_no_data_fails(self):
+        """
+        Create new User without data.
+        Response should be 400 Bad Request.
+        """
         response = self.client.post(self.url, {})
         eq_(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_post_request_with_valid_data_succeeds(self):
-        response = self.client.post(self.url, self.user_data)
+    def test_create_user_with_valid_data_succeeds(self):
+        """
+        Create new User with valid data.
+        Response should be 201 Created.
+        """
+        response = self.client.post(self.url, self.status_data)
         eq_(response.status_code, status.HTTP_201_CREATED)
 
         user = User.objects.get(pk=response.data.get('id'))
-        eq_(user.username, self.user_data.get('username'))
-        ok_(check_password(self.user_data.get('password'), user.password))
+        eq_(user.username, self.status_data.get('username'))
+        ok_(check_password(self.status_data.get('password'), user.password))
 
 
 class TestUserDetailTestCase(APITestCase):
@@ -40,14 +48,25 @@ class TestUserDetailTestCase(APITestCase):
 
     def setUp(self):
         self.user = UserFactory()
+        self.user_admin = UserAdminFactory()
+
+    def test_retrieve_user(self):
+        """
+        Retrieve User with valid data.
+        Response should be 200 OK.
+        """
         self.url = reverse('user-detail', kwargs={'pk': self.user.pk})
         self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.user.auth_token}')
-
-    def test_get_request_returns_a_given_user(self):
         response = self.client.get(self.url)
         eq_(response.status_code, status.HTTP_200_OK)
 
-    def test_put_request_updates_a_user(self):
+    def test_update_user(self):
+        """
+        Update User with valid data.
+        Response should be 200 OK and User first_name should be updated.
+        """
+        self.url = reverse('user-detail', kwargs={'pk': self.user.pk})
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.user.auth_token}')
         new_first_name = fake.first_name()
         payload = {'first_name': new_first_name}
         response = self.client.put(self.url, payload)
