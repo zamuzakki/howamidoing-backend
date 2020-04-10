2634ad7ffd9ec9b5876671f12f8760a50453b84c
Zulfikar Akbar Muzakki <age.zakki@gmail.com>
2020-04-10 17:43:33 +0700

    Edit report.test.test_serializers, add test case for Report Serializer.

--- project/report/test/test_serializers.py
+++ project/report/test/test_serializers.py
@@ -1,20 +1,31 @@
 from django.test import TestCase
 from django.forms.models import model_to_dict
-from django.contrib.auth.hashers import check_password
 from nose.tools import eq_, ok_
-from .factories import StatusFactory
-from ..serializers import StatusSerializer
+from .factories import StatusFactory, ReportFactory
+from ..serializers import StatusSerializer, ReportSerializer
 
 
 class TestStatusSerializer(TestCase):
 
     def setUp(self):
-        self.user_data = model_to_dict(StatusFactory.build())
+        self.status_data = model_to_dict(StatusFactory.build())
 
     def test_serializer_with_empty_data(self):
         serializer = StatusSerializer(data={})
         eq_(serializer.is_valid(), False)
 
     def test_serializer_with_valid_data(self):
-        serializer = StatusSerializer(data=self.user_data)
+        serializer = StatusSerializer(data=self.status_data)
         ok_(serializer.is_valid())
+
+class TestReportSerializer(TestCase):
+
+    def setUp(self):
+        self.report_data = model_to_dict(ReportFactory.build())
+
+    def test_serializer_with_empty_data(self):
+        serializer = ReportSerializer(data={})
+        eq_(serializer.is_valid(), False)
+
+    def test_serializer_with_valid_data(self):
+        pass
