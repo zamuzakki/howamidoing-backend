d1bc88f99e6215885ab0bf16104d566b04f4ec48
Zulfikar Akbar Muzakki <age.zakki@gmail.com>
2020-04-10 17:23:59 +0700

    Edit views.py, change UserViewSet into using ModelViewSet
    Previously, the ViewSet is separated in 2 class. One for creating, and one for retrieveing. Combine those class into ViewSet inheriting ModelViewSet, so we can use this view set to list, retrieve, create, and delete.
    
    Also specify permission and serializer based on the action performed.

--- project/users/views.py
+++ project/users/views.py
@@ -1,25 +1,65 @@
-from rest_framework import viewsets, mixins
-from rest_framework.permissions import AllowAny
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
 from .models import User
 from .permissions import IsUserOrReadOnly
 from .serializers import CreateUserSerializer, UserSerializer
 
-class UserViewSet(mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  viewsets.GenericViewSet):
-    """
-    Updates and retrieves user accounts
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (IsUserOrReadOnly,)
 
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+        Show User object details.
+        <br>
+        Parameter <strong>id</strong> is the ID of the User that you want to see.
 
-class UserCreateViewSet(mixins.CreateModelMixin,
-                        viewsets.GenericViewSet):
+    list:
+        Show list of User object.
+        <br>
+        Parameter <strong>page</strong> indicates the page number.
+        Each page consists of 100 objects
+
+    create:
+        Create new User.
+
+    destroy:
+        Delete User object.
+        <br>
+        Parameter <strong>id</strong> is the ID of the User that you want to delete.
+
+    update:
+        Update User object.
+        <br>
+        Parameter <strong>id</strong> is the ID of the User that you want to update.
+
+    partial_update:
+        Update User object.
+        <br>
+        Parameter <strong>id</strong> is the ID of the User that you want to update.
     """
-    Creates user accounts
-    """
+
     queryset = User.objects.all()
-    serializer_class = CreateUserSerializer
-    permission_classes = (AllowAny,)
+
+    def get_serializer_class(self):
+        """
+        Get serializer class for certain action
+        :return: Serializer class
+        """
+        serializer_class = UserSerializer
+        if self.action == 'create':
+            serializer_class = CreateUserSerializer
+        return serializer_class
+
+    def get_permissions(self):
+        """
+        Get permission object for certain action
+        :return: Permission object
+        """
+        permission_classes = []
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        if self.action == 'list' or self.action == 'partial_update' or \
+                self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        elif self.action == 'retrieve' or self.action == 'update':
+            permission_classes = [IsUserOrReadOnly]
+        return [permission() for permission in permission_classes]
