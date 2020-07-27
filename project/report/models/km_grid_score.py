a8369bddc6cdcae609fffa094eeb4d7fa207ff00
Zulfikar Akbar Muzakki <age.zakki@gmail.com>
2020-07-27 11:34:14 +0700

    Merge develop

--- project/report/models/km_grid_score.py
+++ project/report/models/km_grid_score.py
@@ -264,7 +264,7 @@ class KmGridScore(models.Model):
     )
 
     objects = KmGridScoreManager()
-    vector_tiles = MVTManager(geo_col='geometry')
+    vector_tiles = KmGridScoreMVTManager(geo_col='geometry')
 
     def __str__(self):
         return '{} | {} | {} | {}'.format(self.id, self.geometry, self.population, self.total_score)
